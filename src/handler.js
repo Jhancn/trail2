339f29424bbc9aedfd9c5a655a252d0c932d3000
Jhancn <jhancn39@gmail.com>
2023-11-16 17:04:33 +0530

    second commit

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,39 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.home = async (event) => {
+  // Logic for another endpoint
+  // Handle the request for the 'another-endpoint' path here
+  // Example response:
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "text/plain",
     },
-    body: "Welcome to CG Home page",
+    body: "Welcome to  generateRandomNumber home page!!!!",
   };
- 
+};
+
+module.exports.generateRandomNumber = async (event) => {
+  const randomNumber = parseInt(Math.random()*100);
+  console.log("Random generated Number is",randomNumber);
+  
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ number: randomNumber }),};
+ };
+
+ module.exports.owner = async (event) => {
+  // Logic for another endpoint
+  // Handle the request for the 'another-endpoint' path here
+  // Example response:
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: "Jhansi",
+  };
 };
