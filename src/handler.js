d28d6e879ec88ff81b039823e0416a3efe3d3356
Jhancn <jhancn39@gmail.com>
2023-11-16 18:17:44 +0530

    .

--- src/handler.js
+++ src/handler.js
@@ -9,7 +9,7 @@ module.exports.home = async (event) => {
     headers: {
       "Content-Type": "text/plain",
     },
-    body: "Welcome to  generateRandomNumber home page!!!!!",
+    body: "Welcome to  CG Home page!!!!!",
   };
 };
 
